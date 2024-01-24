f1e819a27bedc2f01078f1cc23b8a9ba4635faf7
Shalom Orangi <shalom.orangi@student.moringaschool.com>
2024-01-25 01:01:09 +0300

    done

--- server/app.py
+++ server/app.py
@@ -2,6 +2,7 @@
 
 from flask import Flask, make_response, jsonify
 from flask_migrate import Migrate
+from sqlalchemy import desc
 
 from models import db, Bakery, BakedGood
 
@@ -24,15 +25,28 @@ def bakeries():
 
 @app.route('/bakeries/<int:id>')
 def bakery_by_id(id):
-    return ''
+    bakery=Bakery.query.get(id)
+    if bakery:
+        serialized_bakery=bakery.to_dict(nested=True)
+        return jsonify(serialized_bakery)
+    else:
+        return jsonify({"message":"Bakery not found"}),404
 
 @app.route('/baked_goods/by_price')
 def baked_goods_by_price():
-    return ''
+    baked_goods=BakedGood.query.order_by(desc(BakedGood.price)).all()
+    serialized_baked_goods=[good.to_dict()for good in baked_goods]
+    return jsonify(serialized_baked_goods)
 
 @app.route('/baked_goods/most_expensive')
 def most_expensive_baked_good():
-    return ''
+    most_expensive_good=BakedGood.query.order_by(desc(BakedGood.price)).first()
+    if most_expensive_good:
+        serialized_most_expensive_good=most_expensive_good.to_dict()
+        return jsonify(serialized_most_expensive_good)
+    
+    else:
+        return jsonify({"message":"NO baked goods found"}),404
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
